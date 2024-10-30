4445453b02911f05df42c466cb9c30307f21a51d
echao <echao8@uwo.ca>
2024-10-30 13:36:58 -0400

    Separating and adding multiple solutions for missing number

--- arrays_and_hashing/easy/missingNumber.py
+++ arrays_and_hashing/easy/missingNumber.py
@@ -3,19 +3,25 @@
 # Difficulty: easy
 # Author: Elbert C.
 
-class Solution:
+# SLOWER SOLUTION
+class Solution1:
     def missingNumber(self, nums: List[int]) -> int:
         # This following solution works, but the time complexity is
         # O(nlogn) which is quite slow:
 
-        # nums.sort()
+        nums.sort() # sort the array of numbers, using O(n) time
 
-        # for index, value in enumerate(nums):
-        #     if(index != value):
-        #         return value-1
+        for index, value in enumerate(nums):
+            if(index != value): # this lists the array as an enumeration {[1, 1], [2, 2]}...
+                                # where you get the index beside it's value, so every number should match if there is nothing missing
+                return value-1
 
-        # return len(nums)
+        return len(nums) # all numbers aren't missing except the last number which would have been the length of the array
 
+
+# FASTER SOLUTION
+class Solution2:
+    def missingNumber(self, nums: List[int]) -> int:
         # This solution is a lot better and faster with a time complexiy of
         # O(n)
         return sum(range(len(nums)+1)) - sum(nums)
