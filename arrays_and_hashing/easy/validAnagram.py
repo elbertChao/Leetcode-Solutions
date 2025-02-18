0a7b7a5dce9c2980b7091042d92fbf998deb7550
echao <echao8@uwo.ca>
2025-02-18 14:47:29 -0500

    better solution for valid anagram

--- arrays_and_hashing/easy/validAnagram.py
+++ arrays_and_hashing/easy/validAnagram.py
@@ -6,24 +6,17 @@
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # if the strings are of different length, automatically not an anagram
-        if len(s) != len(t):
-            return False
+        # make 2 hash tables, one for 's' and 't'
+        countS = {}
+        countT = {}
 
-        # use a hashset, one for string S and one for string T
-        # keeps track of the number of occurances of each letter
-        countOfS = {}
-        countOfT = {}
+        if len(s) != len(t): return False # count of each letter cannot be equal
 
-        for i in range(len(s)):
-            # assign the current value of the occurance of the letter to increase by 1
-            # or if it hasn't appeared in the set yet, set it to 0
-            countOfS[s[i]] = 1 + countOfS.get(s[i], 0)
-            countOfT[t[i]] = 1 + countOfT.get(t[i], 0)
+        # check if the # of occurances of each letter are the same in both sets
+        for x in range(len(s)):
+            countS[s[x]] = 1 + countS.get(s[x], 0)
+            countT[t[x]] = 1 + countT.get(t[x], 0)
 
-        for j in countOfS:
-            # if the count of occurances of a letter in S is not equal to T's string count
-            if countOfS[j] != countOfT.get(j, 0): return False
-        
-        return True
+        # return if they're the same meaning they have the same keys with same values
+        return countS == countT
             
