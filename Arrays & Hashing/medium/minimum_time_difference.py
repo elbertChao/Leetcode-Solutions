cc94488f1f29d4d82f15f8b92da5144d92bd211e
echao <echao8@uwo.ca>
2025-07-23 22:17:21 -0400

    bucket sorting method for min time differences also added

--- Arrays & Hashing/medium/minimum_time_difference.py
+++ Arrays & Hashing/medium/minimum_time_difference.py
@@ -6,21 +6,55 @@
 
 from typing import List
 
+# O(nlogn) Solution
+# class Solution:
+#     def findMinDifference(self, timePoints: List[str]) -> int:
+#         timePoints.sort() # O(nlogn) TC for sorting
 
+#         def time_to_mins(time):
+#             h, m = map(int, time.split(":")) # applies the int() function to each term after splitting it
+#             return h*60 + m
+
+#         # use the first iteration which is the time difference between the last time item and the first item
+#         min_t = (24*60 - time_to_mins(timePoints[-1])) + (time_to_mins(timePoints[0]))
+
+#         for i in range(len(timePoints) - 1): # only need to iterate until the total length - 1 since we already did the last and first term
+#             m = time_to_mins(timePoints[i+1]) - time_to_mins(timePoints[i])
+#             if m == 0:
+#                 return 0
+#             if m < min_t:
+#                 min_t = m
+            
+#         return min_t
+    
+
+# O(n) time with BUCKET SORTING
 class Solution:
     def findMinDifference(self, timePoints: List[str]) -> int:
-        timePoints.sort() # O(nlogn) TC for sorting
-
         def time_to_mins(time):
             h, m = map(int, time.split(":")) # applies the int() function to each term after splitting it
             return h*60 + m
+        
+        buckets = [False] * (24*60) # 1440 buckets
+        first_m, last_m = 24*60, 0
+
+        for t in timePoints:
+            mins = time_to_mins(t)
+            if buckets[mins]:
+                return 0
+            buckets[mins] = True
+            first_m = min(first_m, mins)
+            last_m = max(last_m, mins)
 
         # use the first iteration which is the time difference between the last time item and the first item
-        min_t = (24*60 - time_to_mins(timePoints[-1])) + (time_to_mins(timePoints[0]))
+        ans = (24*60 - last_m) + (first_m)
 
-        for i in range(len(timePoints) - 1): # only need to iterate until the total length - 1 since we already did the last and first term
-            t = time_to_mins(timePoints[i+1]) - time_to_mins(timePoints[i])
-            if t < min_t:
-                min_t = t
+        prev_m = first_m
+        for mins in range(first_m + 1, len(buckets)): # only need to iterate until the total length - 1 since we already did the last and first term
+            if buckets[mins]:
+                diff_mins = mins - prev_m
+                ans = min(ans, diff_mins)
+                prev_m = mins
             
-        return min_t
+            
+        return ans
